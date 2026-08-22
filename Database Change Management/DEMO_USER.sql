b5151e3358d3943285524adb30e62573e7934cf2
Data-2-Dollars <jaswinderkhalsa168202@gmail.com>
2026-08-22 19:44:59 +0530

    Create DEMO_USER.sql

---
+++ Database Change Management/DEMO_USER.sql
@@ -0,0 +1,9 @@
+Select * from HELLO;
+
+select * from DEMO_DB.SCHEMACHANGE.CHANGE_HISTORY; --2 R 2 A
+
+select * from HELLO_WORLD;
+
+select * from V_HELLO_WORLD;
+
+select * from TIME_CAP;--2026-08-22 07:02:34.031   2026-08-22 07:07:00.704
